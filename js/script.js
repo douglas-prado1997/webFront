d4e875cbecc192b0aeea2ec912d2797dbfbb599e
Douglas Prado <douglas.prado@forlogic.net>
2024-06-19 18:28:40 -0300

    adicionando no localStorage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-function aviso() {
-    alert("Cadastrado");
+function register() {
+    const username = document.getElementById('username').value;
+    const usermail = document.getElementById('usermail').value;
+    const id = Date.now().toString();
+    const date = new Date().toISOString();
+    addFormDataToLocalStorage(id, date, username, usermail);
+}
+
+function addFormDataToLocalStorage(id, date, name, email) {
+    const storageKey = 'DadosWebFront';
+    let dataList = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+    const newEntry = {
+        id: id,
+        date: date,
+        name: name,
+        email: email
+    };
+    dataList.push(newEntry);
+
+    localStorage.setItem(storageKey, JSON.stringify(dataList));
 }
